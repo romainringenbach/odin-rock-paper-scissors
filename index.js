6e22e4bbd860fd7018d364560a20f77d75341980
romainringenbach <5659380+romainringenbach@users.noreply.github.com>
2024-04-12 14:50:37 +0200

    Add player's choice function

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-const choices = ["Rock","Scissors","Paper"];
+const choices = ["rock","scissors","paper"];
 
 function getComputerChoice()
 {
     return choices[Math.floor(Math.random()*3)];
+}
+
+function askThePlayer()
+{
+    let choicesAsString = JSON.stringify(choices);
+    let isValid = false;
+    let choice = "";
+
+    while (!isValid){
+        choice = prompt(`Please choose from ${choicesAsString} or quit by entering "quit"`);
+        choice = choice.toLowerCase();
+        if(choice === "quit")
+            return "";
+    
+        isValid = choices.find((element) => element === choice);
+    }
+
+    return choice;
 }
